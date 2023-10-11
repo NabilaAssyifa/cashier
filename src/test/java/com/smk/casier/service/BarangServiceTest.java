c26a66cafbff67525bdbb7c45ad934794256abad
NabilaAssyifa <assyifa703.na@gmail.com>
2023-10-11 14:35:16 +0700

    Adding and Innite Barang Service Pertemuan 9

--- src/test/java/com/smk/casier/service/BarangServiceTest.java
+++ src/test/java/com/smk/casier/service/BarangServiceTest.java
@@ -1,11 +1,13 @@
 package com.smk.casier.service;
 
+import com.smk.casier.dao.BarangDao;
 import com.smk.casier.model.Barang;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +22,27 @@ class BarangServiceTest {
     void getBarangList() {
         List<Barang> barangList=BarangService.getInstance().getBarangList();
         assertEquals(barangList.size(),2);
+    }
+
+    @Test
+    @Order(4)
+    void saveBarangToDatabase(){
+        BarangDao barangDao = new BarangDao();
+        Barang laptop = new Barang();
+        laptop.setKodeBarang("LP001");
+        laptop.setNamaBarang("Laptop");
+        laptop.setHargaBarang(5000000);
+        laptop.setDateCreated(new Date());
+        laptop.setLastModified(new Date());
+        barangDao.save(laptop);
+
+        Barang mouse = new Barang();
+        mouse.setKodeBarang("M0001");
+        mouse.setNamaBarang("Mouse");
+        mouse.setHargaBarang(100000);
+        mouse.setDateCreated(new Date());
+        mouse.setLastModified(new Date());
+        barangDao.save(mouse);
     }
 
     @Test
